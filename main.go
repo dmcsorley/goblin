4b59840e42a2f942d3ef77863b0ab41f36a7c1b5
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-03 00:21:01 -0400

    read config.txt for #2

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"bufio"
         "log"
         "net/http"
         "net/http/httputil"
+	"os"
 )
 
+const (
+	CONFIG_FILE = "config.txt"
+)
+
+func loadConfig() ([]string, error) {
+	file, err := os.Open(CONFIG_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var lines []string
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+	return lines, nil
+}
+
 func main() {
+	cfg, err := loadConfig()
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+	log.Println(cfg)
+
         http.HandleFunc("/",  func(w http.ResponseWriter, r *http.Request) {
 		bytes, err := httputil.DumpRequest(r, true)
-
 		if err != nil {
 			log.Println(err)
 			return
