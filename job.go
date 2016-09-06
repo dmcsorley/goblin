8f86feddc5fe9b8ebe3c525c0d5faee7484bc500
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-06 00:40:40 -0400

    Job.Run with step loop and git-clone step for #7

--- job.go
+++ job.go
@@ -1,27 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
 	"time"
 )
 
 type Job struct {
 	Id string
-	GitURL string
-	job string
 	received time.Time
+	buildConfig *BuildConfig
 }
 
-func NewJob(job string, t time.Time, bc *BuildConfig) *Job {
+func joblog(prefix string, message string, w io.Writer) {
+	io.WriteString(
+		w,
+		fmt.Sprintf("%s %s %s\n",
+			time.Now().Format(time.RFC3339),
+			prefix,
+			message,
+		),
+	)
+}
+
+func pipe(prefix string, rc io.ReadCloser, w io.Writer) {
+	s := bufio.NewScanner(rc)
+	for s.Scan() {
+		joblog(prefix, s.Text(), w)
+	}
+}
+
+func NewJob(t time.Time, bc *BuildConfig) *Job {
 	hasher := sha1.New()
 	hasher.Write([]byte(t.Format(time.RFC3339Nano)))
-	hasher.Write([]byte(job))
+	hasher.Write([]byte(bc.Name))
 	id := hex.EncodeToString(hasher.Sum(nil))
 	return &Job{
 		Id: id,
-		GitURL: bc.Steps[0]["url"],
-		job: job,
 		received: t,
+		buildConfig: bc,
+	}
+}
+
+func cloneStep(dir string, stepPrefix string, cloneConfig map[string]string) error {
+	cmd := exec.Command("git", "clone", "--progress", cloneConfig["url"])
+	cmd.Dir = dir
+	cmdout, _ := cmd.StdoutPipe()
+	cmderr, _ := cmd.StderrPipe()
+	go pipe(stepPrefix, cmdout, os.Stdout)
+	go pipe(stepPrefix, cmderr, os.Stderr)
+
+	time.Sleep(time.Second)
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	return cmd.Wait()
+}
+
+func (job *Job) Run() {
+	joblog(job.Id, "STARTING", os.Stdout)
+	var err error
+	err = os.Mkdir(job.Id, os.ModeDir)
+	if err != nil {
+		joblog(job.Id, fmt.Sprintf("ERROR %v", err), os.Stdout)
+		return
+	}
+
+	cmdPrefix := job.Id[0:20]
+
+	Steps: for i, s := range job.buildConfig.Steps {
+		stepPrefix := cmdPrefix + "-" + strconv.Itoa(i)
+		joblog(stepPrefix, s["type"], os.Stdout)
+		switch s["type"] {
+		case "git-clone":
+			err = cloneStep(job.Id, stepPrefix, s)
+			if err != nil {
+				break Steps
+			}
+		default:
+			err = errors.New(fmt.Sprintf("Unknown step %v", s))
+			break Steps
+		}
+	}
+
+	if err != nil {
+		joblog(job.Id, fmt.Sprintf("ERROR %v", err), os.Stdout)
+	} else {
+		joblog(job.Id, "SUCCESS", os.Stdout)
 	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"os"
-	"os/exec"
 	"strings"
 	"time"
 )
@@ -16,55 +11,6 @@ import (
 const (
 	CONFIG_FILE = "config.json"
 )
-
-func joblog(prefix string, message string, w io.Writer) {
-	io.WriteString(
-		w,
-		fmt.Sprintf("%s %s %s\n",
-			time.Now().Format(time.RFC3339),
-			prefix,
-			message,
-		),
-	)
-}
-
-func pipe(prefix string, rc io.ReadCloser, w io.Writer) {
-	s := bufio.NewScanner(rc)
-	for s.Scan() {
-		joblog(prefix, s.Text(), w)
-	}
-}
-
-func runJob(job *Job) {
-	joblog(job.Id, "STARTING", os.Stdout)
-	direrr := os.Mkdir(job.Id, os.ModeDir)
-	if direrr != nil {
-		log.Println(direrr)
-		return
-	}
-
-	cmdPrefix := job.Id[0:20]
-	cmd := exec.Command("git", "clone", "--progress", job.GitURL)
-	cmd.Dir = job.Id
-	cmdout, _ := cmd.StdoutPipe()
-	cmderr, _ := cmd.StderrPipe()
-	go pipe(cmdPrefix, cmdout, os.Stdout)
-	go pipe(cmdPrefix, cmderr, os.Stderr)
-
-	time.Sleep(time.Second)
-	if err := cmd.Start(); err != nil {
-		log.Println(err)
-		return
-	}
-
-	err := cmd.Wait()
-
-	if err != nil {
-		joblog(job.Id, fmt.Sprintf("ERROR %v", err), os.Stdout)
-	} else {
-		joblog(job.Id, "SUCCESS", os.Stdout)
-	}
-}
 
 func isValidRequest(r *http.Request, cfg *ServerConfig) *BuildConfig {
 	if strings.ToUpper(r.Method) != "POST" {
@@ -97,10 +43,10 @@ func main() {
 		dumpRequest(r)
 		bc := isValidRequest(r, cfg)
 		if bc != nil {
-			job := NewJob(r.URL.Path, now, bc)
+			job := NewJob(now, bc)
 			log.Println("Received request to " + r.URL.Path + " for job " + job.Id)
 			w.WriteHeader(http.StatusOK)
-			go runJob(job);
+			go job.Run()
 		} else {
 			log.Println("Bad request for " + r.URL.Path)
 			w.WriteHeader(http.StatusBadRequest)
