b3554e0385b97aa7dead2d979efa49b5b39ed2d1
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-12 23:38:36 -0400

    add debug flag for #22

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ const (
 	CONFIG_FILE = "goblin.json"
 	LISTEN_ADDR = ":80"
 	ENV_IMAGE = "IMAGE"
+	DEBUG_FLAG = "debug"
 	RUN_FLAG = "run"
 	TIME_FLAG = "time"
 )
@@ -31,10 +32,12 @@ func dumpRequest(r *http.Request) string {
 	return string(bytes)
 }
 
+var debugFlag bool
 var runFlag string
 var timeFlag string
 
 func init() {
+	flag.BoolVar(&debugFlag, DEBUG_FLAG, false, "turn on debug mode")
 	flag.StringVar(&runFlag, RUN_FLAG, "", "build to run from config file")
 	flag.StringVar(&timeFlag, TIME_FLAG, "", "timestamp of build to run")
 }
@@ -71,7 +74,9 @@ func serve(cfg *ServerConfig) {
 		log.Println("Build configured on /" + localConfig.Name)
 		posts.HandleFunc("/" + localConfig.Name, func(w http.ResponseWriter, r *http.Request) {
 			now := time.Now()
-			cibuild.Log("DEBUG", dumpRequest(r))
+			if debugFlag {
+				cibuild.Log("DEBUG", dumpRequest(r))
+			}
 			build := cibuild.New(now, &localConfig)
 			cibuild.Log(build.Id, "Received build for " + r.URL.Path)
 			w.WriteHeader(http.StatusOK)
