c59f645ebf8a89c98b733e339582e000f345d545
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-25 15:03:26 -0400

    fix buildfromdeps.sh

--- buildfromdeps.sh
+++ buildfromdeps.sh
@@ -1,7 +1,7 @@
 DIR=/go/src/github.com/dmcsorley/goblin
 docker run -it --rm \
  -w $DIR -v $PWD:$DIR \
- dmcsorley/goblin:partial \
+ dmcsorley/goblin:deps \
  bash -c "go install -v && cp /go/bin/goblin ./bin/"
 
 docker build -t dmcsorley/goblin .
