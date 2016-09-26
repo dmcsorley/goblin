a6ab7e0537fb8206c1cfe6e90c7ffdf4a3cacb04
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-25 23:18:28 -0400

    match new docker cli.Events usage

--- gobdocker/gobdocker.go
+++ gobdocker/gobdocker.go
@@ -2,10 +2,7 @@
 package gobdocker
 
 import (
-	"bufio"
-	"encoding/json"
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
@@ -35,33 +32,27 @@ func ListenForBuildExits(callback func(*ExitedBuild)) {
 	f.Add("label", "goblin.build")
 	f.Add("event", "die")
 
-	eventstream, err := cli.Events(
+	eventchan, errchan := cli.Events(
 		context.Background(),
 		types.EventsOptions{
 			Filters: f,
 		},
 	)
-	if err != nil {
-		panic(err)
+
+	for {
+	select {
+	case event := <- eventchan:
+		callback(&ExitedBuild{
+			Id:          event.Actor.Attributes["goblin.id"],
+			ContainerId: event.Actor.ID,
+			Name:        event.Actor.Attributes["goblin.name"],
+			Time:        event.Actor.Attributes["goblin.time"],
+			Exit:        event.Actor.Attributes["exitCode"],
+		})
+	case err := <- errchan:
+		log.Printf("%v\n", err)
+		return
 	}
-
-	defer eventstream.Close()
-
-	s := bufio.NewScanner(eventstream)
-	for s.Scan() {
-		event := events.Message{}
-		err := json.Unmarshal([]byte(s.Text()), &event)
-		if err != nil {
-			log.Printf("%v\n", err)
-		} else {
-			callback(&ExitedBuild{
-				Id:          event.Actor.Attributes["goblin.id"],
-				ContainerId: event.Actor.ID,
-				Name:        event.Actor.Attributes["goblin.name"],
-				Time:        event.Actor.Attributes["goblin.time"],
-				Exit:        event.Actor.Attributes["exitCode"],
-			})
-		}
 	}
 }
 
