4163dccda66b88a4c68a6b141f118f6cd641853b
Daniel McSorley <mcsorley.1@gmail.com>
2016-10-01 12:07:47 -0400

    build exactly the image specified, drop unique tag for now

--- cibuild/docker_build_step.go
+++ cibuild/docker_build_step.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/dmcsorley/goblin/command"
 	"github.com/dmcsorley/goblin/config"
-	"github.com/dmcsorley/goblin/gobdocker"
 	"os/exec"
 )
 
@@ -30,7 +29,7 @@ func (dbs *DockerBuildStep) Step(build *Build) error {
 		"build",
 		"--force-rm",
 		"-t",
-		dbs.Image+":"+pfx,
+		dbs.Image,
 		".",
 	)
 	cmd.Dir = WorkDir
@@ -38,10 +37,5 @@ func (dbs *DockerBuildStep) Step(build *Build) error {
 }
 
 func (dbs *DockerBuildStep) Cleanup(build *Build) {
-	pfx := build.stepPrefix(dbs.Index)
-	fmt.Println(pfx, "removing intermediate image")
-	err := gobdocker.RemoveImage(dbs.Image + ":" + pfx)
-	if err != nil {
-		fmt.Println(pfx, err)
-	}
+	// intentionally left blank
 }
